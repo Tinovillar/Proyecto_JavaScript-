f4327c13e7d25334952212c88ad2a08842987d1a
Valentino <valennvillarQ@gmail.com>
2021-01-30 00:41:45 -0300

    objeto carrito terminado, necesita ajustes

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ let burgerInfo = [];
 
 const carrito = new Carrito();
 const burger = new Burger();
+
+carrito.updateCart();
--- carrito.js
+++ carrito.js
@@ -6,54 +6,77 @@ class Carrito {
   total = document.getElementsByClassName("carrito__total");
   precioTotal = 0;
 
-// Local Storage
+// LOCAL STORAGE
   cartSetLocalStorage(){
     localStorage.setItem("carrito", JSON.stringify(cartContainer));
   }
   cartGetLocalStorage(){
     localStorage.getItem("carrito", JSON.parse(cartContainer));
   }
-// 
+//------------------------------------------------------------- 
 
-// Cart Nav 
+// CART NAV
   addToCartNav(){
     cartContainer.push(burgerInfo);
     this.cartSetLocalStorage;
+    this.addToCart;
     this.updateCart;
+    this.totalPrice;
   }
   deleteToCartNav(){
     /*agregar un submenu al carrito nav y agregar un btn para sacar
     el pedido*/
   }
   removeAllToCartNav(){
-    cartContainer = [];
-    this.cartSetLocalStorage;
+    this.removeAllToCart;
     this.updateCart;
+    this.totalPrice;
   }
-//
+//-------------------------------------------------------------
 
-// Actualizar Carrito 
+// ACTUALIZAR CARRITO 
   updateCart(){
     total[0].innerHTML = `Total: $${precioTotal}`;
     carrito__pedidos[0].innerHTML = pedidos;
     li__contador[0].innerHTML = this.cartContainer.length;
   }
-//
+//-------------------------------------------------------------
 
-// Pedidos Carrito
+// PEDIDOS CARRITO
   addToCart(){
     let pedidos = "";
-    
+    if (localStorage.cartContainer != undefined){
+      this.cartGetLocalStorage;
+      for(let i in cartContainer){
+      pedidos += `
+      <div class="pedidos__pedido">
+      <h3 class="pedido__title">${burgerInfo.nombre}</h3>
+      <p class="pedido__descripcion">${burgerInfo.ingredientes}</p>
+      <input type="number" id="cantidad" name="cantidad" min="0" max="10" value="1">
+      <p class="pedido__precio">$${burgerInfo.precio}</p>
+      </div>
+      `
+      }
+    }    
   }
   deleteToCart(){
-    
+    // Agregar btn a los pedidos del carrito para elimiinar uno en especifico
   }
   removeAllToCart(){
-
+    cartContainer = [];
+    precioTotal = 0;
+    this.cartSetLocalStorage;
   }
-  multiplier(){
-
+  totalPrice(){
+    this.cartGetLocalStorage.forEach(burgerPrecio => {
+      let precioEach = burgerInfo.precio;
+      cantidad = document.getElementById("cantidad");
+      if(cantidad > 1){
+        precioEach = burgerInfo.precio * cantidad;
+      }
+      precioTotal += precioEach;
+    })
+    this.updateCart;
   }
-// 
-
+//-------------------------------------------------------------
 }
